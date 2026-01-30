d15cabd134ed236715c56835cfcbede6af5d592f
Artem <artem@wspro.ru>
2026-01-30 14:22:34 +0300

    Head

---
+++ service2/src/main.ts
@@ -0,0 +1,6 @@
+import { LATIN_ONLY_REGEX } from '@dev/api-sdk';
+
+console.info(LATIN_ONLY_REGEX);
+
+setTimeout(() => {}, 10000);
+
---
+++ shared/api-sdk/src/index.ts
@@ -0,0 +1,1 @@
+export const LATIN_ONLY_REGEX = new RegExp('^[A-Za-z]+$');
