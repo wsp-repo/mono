2c46f143981c1dae58a31f96a9695c99c829c687
Artem <artem@wspro.ru>
2026-01-30 15:42:46 +0300

    .

--- service1/src/main.ts
+++ service1/src/main.ts
@@ -1,6 +1,6 @@
 import { LATIN_ONLY_REGEX } from '@dev/api-sdk';
 
-console.info(LATIN_ONLY_REGEX);
+console.info('Service1', LATIN_ONLY_REGEX);
 
 setInterval(() => {
     console.warn('Service1', LATIN_ONLY_REGEX);
--- service2/src/main.ts
+++ service2/src/main.ts
@@ -1,8 +1,8 @@
 import { LATIN_ONLY_REGEX } from '@dev/api-sdk';
 
-console.info(LATIN_ONLY_REGEX);
+console.info('Service2', LATIN_ONLY_REGEX);
 
 setInterval(() => {
-    console.warn('Service1', LATIN_ONLY_REGEX);
+    console.warn('Service2', LATIN_ONLY_REGEX);
 }, 10000);
 
